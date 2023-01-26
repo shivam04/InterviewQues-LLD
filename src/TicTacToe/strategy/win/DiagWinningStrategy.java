9ddc23b45f78abaf2de5c8d75981368c3713f9c4
Shivam Sinha <shvmsnha@amazon.com>
2023-01-27 00:00:03 +0530

    tictactoe

--- src/TicTacToe/strategy/win/DiagWinningStrategy.java
+++ src/TicTacToe/strategy/win/DiagWinningStrategy.java
@@ -8,12 +8,22 @@ public class DiagWinningStrategy implements WinningStrategy{
     @Override
     public boolean checkWin(Board board, Move move) {
         Cell curCell = move.getCell();
-        if (curCell.getRow() != curCell.getCol())
-            return false;
+        boolean win = true;
         for (int i=0; i<board.getN(); i++) {
             Cell cell = board.getCell(i, i);
-            if (cell.getPlayer() != curCell.getPlayer())
-                return false;
+            if (cell.getPlayer() != curCell.getPlayer()) {
+                win = false;
+                break;
+            }
+        }
+        if (!win) {
+            int col = board.getN()-1;
+            for (int row = 0; row < board.getN(); row++, col--) {
+                Cell cell = board.getCell(row, col);
+                if (cell.getPlayer() != curCell.getPlayer()) {
+                    return false;
+                }
+            }
         }
         return true;
     }
